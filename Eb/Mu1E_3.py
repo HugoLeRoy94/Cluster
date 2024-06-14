f7f225a178fe04944a487c9d320424c58cdfa688
cachetdaspirine <leroyhugo@yahoo.fr>
2024-06-14 14:15:10 +0200

    add the simulation to get the influence of Eb, for different mu

---
+++ Eb/Mu1E_3.py
@@ -0,0 +1,90 @@
+
+import time
+start_time =  time.time()
+import numpy as np
+from scipy.special import erfc
+import sys
+sys.path.append('/home/hcleroy/PostDoc/aging_condensates/Simulation/Parallel_Simulation')
+sys.path.append('/home/hcleroy/Parallel_gillespie')
+output_filename = sys.argv[1]
+
+media =""
+
+import Parallel_Run
+from Cluster import Cluster
+from ISF import ISF
+from MSD import MSD
+from Energy import NRG
+from PCF import PCF
+from PCF import PCF_L
+from Time import Time
+
+# gillespie parameter
+Nlinker = 100
+ell_tot = 1*10**5
+kdiff = 0.001
+Energy = -15
+
+
+Nprocess = 100
+seeds = set()
+while len(seeds) < Nprocess:
+    seeds.add(np.random.randint(1000000))
+seeds = list(seeds)
+args = [[ell_tot,Energy,kdiff,seeds[_],Nlinker,3] for _ in range(Nprocess)]
+
+# argument of the different classes
+avR = lambda L,N : 2*(np.exp(-1.5/(L/N)) * np.sqrt(L/N*6/np.pi)*(3+2*L/N) - 9*erfc(np.sqrt(3/2/(L/N))))/(9*L/N) #average distance between equilibrated nodes
+Lcharact = lambda L,N : (np.sqrt(2*L/N/3))
+cluster_arg = tuple([Lcharact(ell_tot,Nlinker)]) # max distance
+MSD_arg = () # no argument 
+ISF_arg = (1/avR(ell_tot,Nlinker),10) # q_norm, q_num_sample
+NRG_arg = ()
+PCF_arg = (15,50) # max_distance,numb_bin
+PCF_L_arg = (ell_tot,30) # max_distance,numb_bin
+Time_arg = ()
+
+measurement_args = {
+    'Cluster': (Cluster, cluster_arg),
+    'MSD': (MSD, MSD_arg),
+    'ISF': (ISF, ISF_arg),
+    'PCF':(PCF,PCF_arg),
+    'PCF_L':(PCF_L,PCF_L_arg),
+    'NRG':(NRG,NRG_arg)#,
+    #'Time':(Time,Time_arg)
+    # Add other measurements as needed
+}
+
+measurement_flags = {
+    'NRG':True,
+    'Cluster': False,
+    'MSD': False,
+    'ISF': True,
+    'PCF':True,
+    'PCF_L':False#,
+    #'Time':True
+    # Set each measurement to True/False as desired
+}
+
+# Simulation parameters
+step_tot = 5*10**5
+#check_steps = 10**2
+initial_check_steps = 10**2
+coarse_grained_step = 10**1
+log_base=None
+
+Parallel_Run.parallel_evolution(args,step_tot,initial_check_steps,coarse_grained_step,media+output_filename+'.hdf',
+                                measurement_args,measurement_flags,log_base)
+
+
+duration = time.time()-start_time
+# Convert seconds to days, hours, minutes, and seconds
+days = int(duration // (24 * 3600))
+duration = duration % (24 * 3600)
+hours = int(duration // 3600)
+duration %= 3600
+minutes = int(duration // 60)
+seconds = int(duration % 60)
+
+# Print the formatted duration
+print(f"Execution Time: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
---
+++ Eb/Mu1E_3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Define the Python script filename
+PYTHON_FILE="Mu1E_3.py"
+
+# Extract parameter values using grep and cut
+Nlinker=$(grep '^Nlinker\s*=' $PYTHON_FILE | cut -d '=' -f2 | tr -d '[:space:]')
+ell_tot=$(grep '^ell_tot\s*=' $PYTHON_FILE | cut -d '=' -f2 | tr -d '[:space:]')
+kdiff=$(grep '^kdiff\s*=' $PYTHON_FILE | cut -d '=' -f2 | tr -d '[:space:]')
+Energy=$(grep '^Energy\s*=' $PYTHON_FILE | cut -d '=' -f2 | tr -d '[:space:]')
+
+# Python code to format the parameters into a file name
+FORMAT_SCRIPT=$(cat <<EOF
+def scientific_format(num):
+    if num == 0:
+        return "0"
+    sci_notation = f"{num:.0e}".split('e')
+    coefficient = int(sci_notation[0])
+    exponent = int(sci_notation[1])
+    if exponent == 0:
+        return f"{coefficient}"
+    elif coefficient == 1:
+        return f"E{exponent}"
+    else:
+        return f"{coefficient}E{exponent}"
+
+nlinker_formatted = f"N{$Nlinker}"
+energy_formatted = f"E{$Energy}"
+ell_tot_formatted = f"ell{scientific_format(float($ell_tot))}"
+kdiff_formatted = f"kdiff{scientific_format(float($kdiff))}"
+
+result = f"{energy_formatted}_{nlinker_formatted}_{ell_tot_formatted}_{kdiff_formatted}"
+print(result)
+EOF
+)
+
+# Execute Python script and capture output into a Bash variable
+formatted_output=$(python3 -c "$FORMAT_SCRIPT")
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --job-name="$formatted_output"
+#SBATCH --output=logs/${formatted_output}_output.txt
+#SBATCH --error=logs/${formatted_output}_error.txt
+#SBATCH --time=50:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=1G
+
+# Load the module
+module load gcc python
+
+# Activate the virtual environment
+source /home/hcleroy/venvs/base/bin/activate
+
+# Run your Python script
+srun python $PYTHON_FILE $formatted_output
+
+deactivate
+EOT
